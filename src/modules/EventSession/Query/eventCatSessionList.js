b438f1f384c75159c2b007d6847f302d21487b07
sebabouche <sebastien@sifnos.io>
2018-03-27 17:46:57 +0200

    eventCatsSessionList

---
+++ src/modules/EventSession/Query/eventCatSessionList.js
@@ -0,0 +1,16 @@
+import { ObjectId } from "mongodb"
+
+const eventCatSessionList = async ({ eventCatId }, { mongo: { EventModels, EventSessions } }) => {
+  const catId = ObjectId(eventCatId)
+  const eventModels = await EventModels.find({ "eventCats.id": catId }).toArray()
+  const eventModelIds = eventModels.map(em => em._id)
+  const eventSessions = await EventSessions.find({
+    $or: [
+      { "eventCatsSuper.id": catId, published: true },
+      { eventModelId: { $in: eventModelIds }, published: true },
+    ],
+  }).toArray()
+  return eventSessions
+}
+
+export default eventCatSessionList
