c0ed598dfc88076bb5f82e0371fc0e19608b9bdd
sebabouche <sebastien@sifnos.io>
2018-07-08 18:15:40 +0200

    Mailer update

--- src/modules/Newcomer/mailer.js
+++ src/modules/Newcomer/mailer.js
@@ -8,11 +8,15 @@ export const sendNewcomerDigits = async (newcomer, mailer) => {
   }
   const firstDelivery = {
     subject: "Wynn: your code to activate your account",
-    html: `<p>Your code is:</p><h1>${newcomer.digits}</h1><p>To continue the signup process, <a href='${CLIENT_URI}/connect/signup/enterdigits'>write this code on this page</a></p>`,
+    html: `<p>Your code is:</p><h1>${
+      newcomer.digits
+    }</h1><p>To continue the signup process, <a href='${CLIENT_URI}/connect/signup/enterdigits'>write this code on this page</a></p>`,
   }
   const nextDeliveries = {
     subject: "Wynn: here is another code to activate your account",
-    html: `<p>Your new code is:</p><h1>${newcomer.digits}</h1><p>To continue the signup process, <a href='${CLIENT_URI}/connect/signup/enterdigits'>write this code on this page</a></p>`,
+    html: `<p>Your new code is:</p><h1>${
+      newcomer.digits
+    }</h1><p>To continue the signup process, <a href='${CLIENT_URI}/connect/signup/enterdigits'>write this code on this page</a></p>`,
   }
   const data = () => {
     if (newcomer.resent) {
@@ -34,8 +38,13 @@ export const sendInvites = async (newcomers, message, mailer) => {
     const data = {
       to: `${newcomer.fullName} <${newcomer.email}>`,
       from: EMAIL_DEFAULT,
-      subject: "Welcome to FAB14 Ecology. Book now the events you want to attend",
-      html: `<h3>Welcome, ${newcomer.fullName}</h3><p>${message}</p><p><a href='${CLIENT_URI}/inscription/token/${newcomer.token}'>Click here to finish signup and choose the events you want to attend.</a></p>`,
+      subject:
+        "Welcome to FAB14 Ecology/Solidarity. Instructions to book the conferences you want to attend",
+      html: `<h3>Welcome, ${
+        newcomer.fullName
+      }</h3><p>${message}</p><p><a href='${CLIENT_URI}/inscription/token/${
+        newcomer.token
+      }'>Click here to create your account and book events you want to attend.</a></p>`,
     }
     await mailer(data)
   })
